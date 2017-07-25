196acdc257569001838bd2fbf94bae0fe4f1e6e6
Kevin Chan <kchan3188@gmail.com>
2017-07-25 17:30:21 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var pg = require(‘pg’);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,6 +18,18 @@ app.get('/messages', function (req, res, next) {
   setTimeout(() => {
     res.json({hello: "World"})
   }, 500);
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
 });
 
 app.get('/clocks', function (req, res, next) {
